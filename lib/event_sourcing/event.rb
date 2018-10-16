4addfa52760e88d8bec71b407aa43ca86cda6110
Tiago Amaro <tiagopadrela@gmail.com>
2018-10-16 15:06:10 -0300

    Create base event class (simplified PORO)

---
+++ lib/event_sourcing/event.rb
@@ -0,0 +1,75 @@
+# This is the base `Event` class that all Events inherit from.
+# It takes care of serializing `data` and `metadata`
+# It defines setters and accessors for the defined `data_attributes`
+# After create, it calls `apply` to apply changes.
+#
+# Subclasses must define the `apply`, `build_aggregate` and `persist` methods
+module EventSourcing
+  class Event
+    attr_reader :data, :metadata
+    attr_accessor :aggregate
+
+    # Define attributes to be serialized in the `data` column.
+    # It generates setters and getters for those.
+    #
+    # Example:
+    #
+    # class MyEvent < Lib::BaseEvent
+    #   data_attributes :title, :description, :drop_id
+    # end
+    #
+    # MyEvent.create!(
+    def self.data_attributes(*attrs)
+      attrs.map(&:to_s).each do |attr|
+        define_method attr do
+          @data[attr.to_sym]
+        end
+
+        define_method "#{attr}=" do |arg|
+          @data[attr.to_sym] = arg
+        end
+      end
+    end
+
+    # @param [Hash] attrs
+    def initialize(attrs = {})
+      @data = attrs
+      @metadata = { klass: self.class.name }
+    end
+
+    # Apply the event to the aggregate passed in.
+    # Must return the aggregate.
+    def apply(_aggregate)
+      raise NotImplementedError
+    end
+
+    # Replays the event on any given aggregate
+    # @param [Object] aggregate
+    def replay(aggregate)
+      event_class = Object.const_get(metadata[:klass])
+      event_class.new(data).apply(aggregate)
+    end
+
+    # Persists and dispatches the event
+    def save
+      build_aggregate
+      persist
+      dispatch
+    end
+
+    private
+
+    def build_aggregate
+      raise NotImplementedError
+    end
+
+    def dispatch
+      EventSourcing::EventDispatcher.dispatch(self)
+    end
+
+    # Persists the transformation event. Must be implemented.
+    def persist
+      raise NotImplementedError
+    end
+  end
+end
---
+++ spec/lib/event_sourcing/event_spec.rb
@@ -0,0 +1,72 @@
+require "spec_helper"
+
+RSpec.describe EventSourcing::Event do
+  class User
+    attr_accessor :name
+  end
+
+  class CreatedEvent < EventSourcing::Event
+  end
+
+  class UpdatedEvent < EventSourcing::Event
+    data_attributes :name
+
+    def apply(user)
+      user.name = name
+      user
+    end
+  end
+
+  describe ".initialize" do
+    let(:instance) { CreatedEvent.new(attributes) }
+    let(:attributes) { { id: 12345, name: "John Doe" } }
+
+    it "initializes event's data" do
+      expect(instance.data).to eq(id: 12345, name: "John Doe")
+    end
+
+    it "initializes event's metadata" do
+      expect(instance.metadata).to eq(klass: "CreatedEvent")
+    end
+  end
+
+  describe "#apply" do
+    let(:aggregate) { User.new }
+    let(:instance) { CreatedEvent.new }
+
+    it { expect { instance.apply(aggregate) }.to raise_error(NotImplementedError) }
+  end
+
+  describe "#replay" do
+    let(:aggregate) { User.new }
+    let(:event) { UpdatedEvent.new(name: "John Doe") }
+
+    it "executes the event on the given aggregate" do
+      event.replay(aggregate)
+      expect(aggregate.name).to eq("John Doe")
+    end
+  end
+
+  describe "#save" do
+    let(:event) { UpdatedEvent.new(name: "Aunt May") }
+
+    before do
+      allow(event).to receive_messages(build_aggregate: true, persist: true)
+      allow(EventSourcing::EventDispatcher).to receive(:dispatch)
+
+      event.save
+    end
+
+    it "calls instance's build aggregate" do
+      expect(event).to have_received(:build_aggregate)
+    end
+
+    it "calls event dispatcher dispatch" do
+      expect(EventSourcing::EventDispatcher).to have_received(:dispatch).with(event)
+    end
+
+    it "calls instance's persist" do
+      expect(event).to have_received(:persist)
+    end
+  end
+end
