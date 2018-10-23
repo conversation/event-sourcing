19240591549c2bdf07004946c5cd4fc4388ab5ab
Tiago Amaro <tiagopadrela@gmail.com>
2018-10-23 09:36:42 -0300

    Create database migrations on tests
    Thanks @nullobject!
    
    Reference: https://github.com/nullobject/rein

---
+++ spec/rails_helper.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+require "active_support"
+require "active_model"
+require "active_record"
+
+require "support/migration"
+
+MIGRATIONS_PATH = [File.expand_path(File.join("rails", "migrations"), __dir__)].freeze
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    ActiveRecord::Base.establish_connection(adapter: "postgresql", database: "event_sourcing_test")
+    ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS schema_migrations")
+    ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS user_events")
+    ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS users")
+    if ActiveRecord.version.release < Gem::Version.new("5.2.0")
+      ActiveRecord::Migrator.migrate(MIGRATIONS_PATH)
+    else
+      ActiveRecord::MigrationContext.new(MIGRATIONS_PATH).migrate
+    end
+  end
+end
